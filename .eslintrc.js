c1bc5b63b458ad4151d3532f1dc1215b421243fe
Kaian Cotias <kcotias@MacBook-Pro-de-Kaian.local>
2021-01-22 14:51:52 -0300

    adds basic project config and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,41 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2020: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react-native/all',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-native', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react-native/no-color-literals': 'off',
+    'react-native/sort-styles': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
+  settings: {},
 };
