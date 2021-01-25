9151977147990b47e9de7fb9032a5ceeb0d9e96e
Kaian Cotias <kaianexperia@gmail.com>
2021-01-25 16:17:08 -0300

    add project default styles

---
+++ src/styles/Colors.ts
@@ -0,0 +1,11 @@
+export default {
+  PRIMARY: '#5D50FE',
+  PRIMARY_DARK: '#fd576b',
+  SECONDARY: '#ffbf6f',
+  TERTIARY: '#fdfc6d',
+  FONT_DARK: '#130E51',
+
+  // FILTER
+  ACTIVE_BUTTON: 'rgba(173,167,254, 0.15)',
+  ACTIVE_TEXT: '#5D50FE',
+};
---
+++ src/styles/Layout.ts
@@ -0,0 +1,14 @@
+import { Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
+
+export default {
+  window: {
+    width,
+    height,
+  },
+  spacing: {
+    paddingX: 16,
+  },
+};
