7447d686fcad809c62ef98764a5ac36e2a90138e
Kaian Cotias <kaianexperia@gmail.com>
2021-01-26 17:11:24 -0300

    add assets

---
+++ src/assets/animations/index.ts
@@ -0,0 +1,16 @@
+const animations = {
+  loading: require('./loading.json'),
+  thunders: require('./thunders.json'),
+  rain: require('./rain.json'),
+  rain_night: require('./rain_night.json'),
+  clouds: require('./clouds.json'),
+  clouds_night: require('./clouds_night.json'),
+  clear: require('./clear.json'),
+  clear_night: require('./clear_night.json'),
+  snow: require('./snow.json'),
+  snow_night: require('./snow_night.json'),
+  atmosphere: require('./atmosphere.json'),
+  loading_weather: require('./loading_weather.json'),
+};
+
+export default animations;
