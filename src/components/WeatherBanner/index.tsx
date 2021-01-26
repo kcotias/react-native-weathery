0ce8aa2164af6fc27ef9c82a7cf6bd39a4fd14cb
Kaian Cotias <kaianexperia@gmail.com>
2021-01-26 17:08:54 -0300

    add weatherbanner component

--- src/components/WeatherBanner/index.tsx
+++ src/components/WeatherBanner/index.tsx
@@ -1,41 +1,54 @@
 import React from 'react';
 import { View, ImageBackground, Text } from 'react-native';
-import { Colors } from 'styles';
-import Icon from 'react-native-vector-icons/Ionicons';
+import LottieView from 'lottie-react-native';
+
+import { capitalizeFirstLetter, setWeatherAnimationAsset } from 'utils/helpers';
+import { WeatherResponse } from 'types';
+import styles from './styles';
+
+const backgroundAsset = require('assets/imgs/background.png');
 
 interface WeatherBannerProps {
-  data?: any;
+  weather: WeatherResponse | null;
+  filter: number | null;
 }
 
-const WeatherBanner: React.FC<WeatherBannerProps> = ({ data }: HeaderProps) => (
-  <View
-    style={{
-      width: 374,
-      height: 314,
-      backgroundColor: Colors.PRIMARY,
-      borderRadius: 40,
-      margin: 20,
-    }}>
-    <ImageBackground
-      style={{ width: 374, height: 314, justifyContent: 'center', alignItems: 'center' }}
-      imageStyle={{ borderRadius: 40, opacity: 0.3 }}
-      source={require('assets/background.png')}>
-      <Text style={{ fontSize: 120, color: 'white' }}>25</Text>
-      <Text style={{ fontSize: 21, marginBottom: 15, color: 'white', marginTop: -10 }}>
-        Clouds & sun
-      </Text>
-      <Text style={{ fontSize: 19, fontWeight: '600', color: 'white', marginBottom: 5 }}>
-        Humidity
-      </Text>
-      <Text style={{ fontSize: 27, fontWeight: '600', color: 'white', opacity: 0.4 }}>35</Text>
-      <Icon
-        style={{ position: 'absolute', bottom: 10, right: 15 }}
-        name="sunny-outline"
-        size={100}
-        color="white"
-      />
-    </ImageBackground>
-  </View>
-);
+const WeatherBanner: React.FC<WeatherBannerProps> = ({ weather, filter }: WeatherBannerProps) => {
+  const weatherSet = {
+    0: weather?.current,
+    1: weather?.daily[1],
+    2: weather?.daily[7],
+  };
+
+  const temperature =
+    typeof weatherSet[filter]?.feels_like === 'object'
+      ? weatherSet[filter]?.feels_like.day
+      : weatherSet[filter]?.feels_like;
+  const weatherDescription = weatherSet[filter]?.weather[0].description;
+  const weatherMainStatus = weatherSet[filter]?.weather[0]?.main;
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.backgroundContainer}
+        imageStyle={styles.background}
+        source={backgroundAsset}>
+        <Text style={styles.temperature}>
+          {temperature.toFixed()}
+          <Text style={styles.celsius}>&#8451;</Text>
+        </Text>
+        <Text style={styles.weatherDescription}>{capitalizeFirstLetter(weatherDescription)}</Text>
+        <Text style={styles.humidityTitle}>Humidity</Text>
+        <Text style={styles.humidityValue}>{weather?.current?.humidity}</Text>
+        <LottieView
+          style={styles.animation}
+          source={setWeatherAnimationAsset(weatherMainStatus)}
+          autoPlay
+          loop
+        />
+      </ImageBackground>
+    </View>
+  );
+};
 
 export default WeatherBanner;
---
+++ src/components/WeatherBanner/styles.ts
@@ -0,0 +1,55 @@
+import { StyleSheet, TextStyle } from 'react-native';
+import { Colors } from 'styles';
+
+export default StyleSheet.create({
+  container: {
+    width: 374,
+    height: 314,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 40,
+    margin: 20,
+  },
+  backgroundContainer: {
+    width: 374,
+    height: 314,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    borderRadius: 40,
+    opacity: 0.3,
+  },
+  temperature: {
+    fontSize: 120,
+    color: 'white',
+  } as TextStyle,
+  celsius: {
+    fontSize: 28,
+    fontWeight: '600',
+  } as TextStyle,
+  weatherDescription: {
+    fontSize: 21,
+    marginBottom: 15,
+    color: 'white',
+    marginTop: -10,
+  } as TextStyle,
+  humidityTitle: {
+    fontSize: 19,
+    fontWeight: '600',
+    color: 'white',
+    marginBottom: 5,
+  } as TextStyle,
+  humidityValue: {
+    fontSize: 27,
+    fontWeight: '600',
+    color: 'white',
+    opacity: 0.4,
+  } as TextStyle,
+  animation: {
+    position: 'absolute',
+    bottom: -15,
+    right: -15,
+    width: 200,
+    height: 200,
+  },
+});
